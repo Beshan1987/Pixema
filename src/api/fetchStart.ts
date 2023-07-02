0db8515cdc13bf918e4dbf3f7e8b33770ed58014
user <user@users-MacBook-Pro.local>
2023-07-02 12:03:47 +0300

    fix: fix view tablet/desktop/mobile devices

--- src/api/fetchStart.ts
+++ src/api/fetchStart.ts
@@ -19,7 +19,7 @@ export async function fetchStart({
   const headers = new AxiosHeaders();
   headers.set('X-API-KEY', `4CTXY50-B5JMC9P-Q6E6KXJ-WRYJH15`);
   const { data } = await axios<ResponseStartPage>(
-    `https://api.kinopoisk.dev/v1.3/movie?year=2015-2023&page=${page}`,
+    `https://api.kinopoisk.dev/v1.3/movie?year=2015-2023&page=${page}&audience.count=1000000-10000000`,
     { headers }
   );
 
--- src/features/Card/Card.utils.ts
+++ src/features/Card/Card.utils.ts
@@ -4,3 +4,15 @@ export function getRatingLevel(rate: number) {
   }
   return rate < 5 ? 'superlow' : 'low';
 }
+
+export function isTrend({
+  year,
+  audienceCount
+}: {
+  year: number;
+  audienceCount: number;
+}) {
+  if (year === 2023 && audienceCount > 10_000) {
+    return 'trend';
+  }
+}
